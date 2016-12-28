981ad4638dc3d267187798434080cb815bb2af82
YoGhurt <623770278@qq.com>
2016-12-28 22:00:28 +0800

    基于朴素贝叶斯的文本分类

---
+++ Bayes/bayes.py
@@ -0,0 +1,75 @@
+#coding=utf-8
+from numpy import *
+
+
+def loadDataSet():  # 读取数据集
+    postinngList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'stupid', 'park'],
+                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                    ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                    ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postinngList, classVec
+
+
+def creatVocabList(dataSet):  # 根据数据集生成词汇表
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+
+def setOfWords2Vec(vocabList, inputSet):  # 将单词映射到向量
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):  # 训练算法
+    numTrainDocs = len(trainMatrix)  # 文档个数
+    numWords = len(trainMatrix[0])  # 总单词数
+    pAbusive = sum(trainCategory) / float(numTrainDocs)  # 侮辱性文档的先验概率
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
+    # 循环算出所有侮辱性和非侮辱性词汇在vocabList中的分布位置以及所有的侮辱性词汇个数p1Denom和非侮辱性词汇个数p0Denom
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1): # 分类
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)  # log(P(Wn|c1)) + log(P(c1))
+    p0 = sum(vec2Classify * p0Vec) + log(1-pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():  # 测试算法
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = creatVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+    testEntry = ['love', 'stupid', 'my','garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)

6783b08c2615d6cfbad3ca9b9b8d6f19db8f61ba
YoGhurt <623770278@qq.com>
2017-01-04 14:58:06 +0800

    含ROC曲线和决策面

--- Fisher/fisher.py
+++ Fisher/fisher.py
@@ -40,10 +40,22 @@ def judge(dataListForBoy,dataListForGirl,w,w0,testLen1,testLen0):
             rightNum0 = rightNum0 + 1
     print "判断女生正确率是",rightNum0/testLen0
 
+def plotROC(items, title, xlable, ylable, legend):
+    for points, color in items:
+        x = [i[0] for i in points]
+        y = [i[1] for i in points]
+        pylab.plot(x,y, color)
+    pylab.title(title)
+    plt.xlabel(xlable)
+    plt.ylabel(ylable)
+    plt.grid(True)
+    plt.legend(legend, loc='upper right')
+    pylab.show()
+
 
 dataList1,lenth1 = loadData("boy.txt")
 dataList0,lenth0 = loadData("girl.txt")
-prioriP1 = 0.5
+prioriP1 = 0.7
 mean1,cov1 = getParam(dataList1)
 mean0,cov0 = getParam(dataList0)
 Sw = np.mat(cov1+cov0)
@@ -53,6 +65,34 @@ m0 = np.dot(w.T,mean0)
 w0 =float((m1+m0)/2 + math.log(prioriP1/(1-prioriP1))/(lenth0+lenth1-2))
 testList1,testLen1 = loadData("boynew.txt")
 testList0,testLen0 = loadData("girlnew.txt")
-judge(testList1,testList0,w,w0,testLen1,testLen0)
+# 散点图
+x = [dataList1[i][0] for i in range(lenth1)]
+y = [dataList1[i][1] for i in range(lenth1)]
+plt.scatter(x, y, s=x*500, c=u'r', marker=u'*')
+x = [dataList0[i][0] for i in range(lenth0)]
+y = [dataList0[i][1] for i in range(lenth0)]
+plt.scatter(x, y, s=x*500, c=u'g', marker=u'o')
+# 决策面
+x = np.linspace(155, 175, 50)
+y = np.array(((w0 - w[0] * x) / w[1]).T)
+plt.plot(x, y)
+plt.show()
+# ROC
+points = []
+for a in np.linspace(float(m0), float(m1), 10):
+    err1 = 0.0
+    err0 = 0.0
+    for i in range(lenth1):
+        if classify(testList1[i], w, a) != 1:
+            err1 = err1 + 1
+    err1Rate = err1 / lenth1
+    for i in range(lenth0):
+        if classify(testList0[i], w, a) != 0:
+            err0 = err0 + 1
+    err0Rate = err0 / lenth0
+    points.append([err0Rate, err1Rate])
+plotROC([[points, 'y']], 'ROC Curves', "girl's error rate", "boy's error rate", ('Fisher', ))
+judge(testList1, testList0, w, w0, testLen1, testLen0)
+
 
 
--- Fisher/test.py
+++ Fisher/test.py
@@ -33,7 +33,6 @@ def loadDataSet():
         for item in f.readlines():
             DataSet.append(map(float, item.split()[:2]))
             classLables.append(0)
-    #print DataSet
     return DataSet, classLables
 
 def loadTestSet():
@@ -49,23 +48,17 @@ def loadTestSet():
     return DataSet, classLables
 
 def getParam(dataSet, classLables):
-    boyData = np.mat([dataSet[i] for i in range(len(dataSet)) if classLables[i]==1])  # n*2
+    boyData = np.mat([dataSet[i] for i in range(len(dataSet)) if classLables[i]==1])
     girlData  = np.mat([dataSet[i] for i in range(len(dataSet)) if classLables[i]==0])
-    mean1 = np.mat((boyData.T[0].mean(), boyData.T[1].mean())).T  # 2*1
-    # print mean1
+    mean1 = np.mat((boyData.T[0].mean(), boyData.T[1].mean())).T
     mean0 = np.mat((girlData.T[0].mean(), girlData.T[1].mean())).T
-    print mean0
-    cov1 = np.mat(np.cov(boyData.T))  # 2*2
-    #print cov1
+    cov1 = np.mat(np.cov(boyData.T))
     cov0 = np.mat(np.cov(girlData.T))
     Sw = np.mat(cov0 + cov1)
-    #print Sw
     w = Sw.I * (mean0 - mean1)
-    #print w
     m1 = np.linalg.det(w.T * mean1)
-    #print m1
     m0 = np.linalg.det(w.T * mean0)
-    return w, m0, m1, cov0, cov1, (m0 + m1) / 2
+    return w, mean0, mean1, m0, m1, cov0, cov1, (m0 + m1) / 2
 
 def classify(data, w, w0):
     if w.T * data > w0:
@@ -74,39 +67,49 @@ def classify(data, w, w0):
         return 1
 
 def pdf(x, pw, mu, cov):
-    x = np.matrix(x)
+    x = np.mat(x)
     return pw * np.exp(-1*np.dot(np.dot((x-mu),cov.I),(x.T-mu.T))/2)/np.sqrt(np.linalg.det(cov))
-
-def classifyB(x, pw, mu, cov, a):
-    p=[1,1]
-    for i in range(2):
-        p[i] = pdf(x, pw[i], mu[i], cov[i])
-    if(p[0]-p[1]> a):
-        return 0
-    else:
-        return 1
 
 pw = {0:0.5,1:0.5}
 dataSet, classLables = loadDataSet()
-w, m0, m1,cov0, cov1, w0 = getParam(dataSet, classLables)
+w, mean0, mean1, m0, m1,cov0, cov1, w0 = getParam(dataSet, classLables)
 for i in range(len(dataSet)):
-    plt.plot(dataSet[i][0], dataSet[i][1], 'ro' if classLables[i]==0 else 'bo')
-# x = np.linspace(145, 190, 100)
-# y = np.array((w[0] * x / w[1]).T)
-# plt.plot(x, y)
-x = np.linspace(145, 190, 100)
+    plt.plot(dataSet[i][0], dataSet[i][1], 'r.' if classLables[i]==0 else 'b.')
+
+x = np.linspace(145, 165, 100)
+y = np.array((-450 + w[0] * x / w[1]).T)
+plt.plot(x, y)
+
+x = np.linspace(155, 175, 50)
 y = np.array(((w0 - w[0] * x )/ w[1]).T)
 plt.plot(x, y)
 
-# delta = 0.1
-# x = np.arange(145, 190, delta)
-# y = np.arange(-20, 140, delta)
-# X, Y = np.meshgrid(x, y)
-# Z0 = mlab.bivariate_normal(X, Y, m0[0, 0], m0[1, 0], cov0[0, 0], cov0[1, 1], cov0[0, 1])
-# Z1 = mlab.bivariate_normal(X, Y, m1[0, 0], m1[1, 0], cov1[0, 0], cov1[1, 1], cov1[0, 1])
-# Z = 10000*(Z1 - Z0)
-# print Z[:10]
-# CS = plt.contour(X, Y, Z0)
-# plt.clabel(CS, inline=1, fontsize=6)
+delta = 1
+x = np.arange(145, 190, delta)
+y = np.arange(30, 90, delta)
+X, Y = np.meshgrid(x, y)
+Z0 = mlab.bivariate_normal(X, Y, np.sqrt(cov0[0, 0]), np.sqrt(cov0[1, 1]), mean0[0, 0], mean0[1, 0], cov0[0, 1])
+Z1 = mlab.bivariate_normal(X, Y, np.sqrt(cov1[0, 0]), np.sqrt(cov1[1, 1]), mean1[0, 0], mean1[1, 0], cov1[0, 1])
+Z = 1000*(Z1 - Z0)
+CS = plt.contour(X, Y, Z)
+plt.clabel(CS, inline=1, fontsize=6)
 
+plt.xlabel('Height')
+plt.ylabel('weight')
 plt.show()
+
+testData, testLable = loadTestSet()
+points = []
+for a in np.linspace(m0, m1, 10):
+    err = [0.0, 0.0]
+    l = [0, 0]
+    for i in range(len(testData)):
+        t = testLable[i]
+        l[t] += 1
+        if classify(np.mat(testData[i]).T, w, a)!=t:
+            err[t] += 1
+    for i in range(2):
+        err[i] /= l[i]
+    points.append(err)
+print points
+plotROC([[points, 'r']], 'ROC Curves', "girl's error rate", "boy's error rate", ('Fisher', ))
